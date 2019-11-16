3a06e1e3e97642e25aba09e1ff14606381da9fe7
Adrienne Yao <adrienne.yao@icloud.com>
2019-11-16 13:32:26 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-# set up connection to a sqlite3 database in a folder called 'db'
 configure :development do
   set :database, 'sqlite3:db/database.db'
 end
-
 require './app'
